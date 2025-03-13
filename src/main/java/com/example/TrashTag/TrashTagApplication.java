d399faca0ec366cd04d8c924746947c3b66ecb30
h679177 <h679177@stud.hvl.no>
2025-03-13 10:46:07 +0100

    Initial commit

---
+++ src/main/java/com/example/TrashTag/TrashTagApplication.java
@@ -0,0 +1,13 @@
+package com.example.TrashTag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrashTagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrashTagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TrashTag/TrashTagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TrashTag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrashTagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
